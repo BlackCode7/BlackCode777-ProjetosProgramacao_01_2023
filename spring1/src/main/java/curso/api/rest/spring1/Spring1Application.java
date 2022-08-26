f1a846c46ae163a565aad62cdbeb4e2aec483daa
Anderson <BlackCode7@outlook.com>
2022-08-26 08:04:06 -0300

    configuração centralizada para liberação do cors origin

--- spring1/src/main/java/curso/api/rest/spring1/Spring1Application.java
+++ spring1/src/main/java/curso/api/rest/spring1/Spring1Application.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"curso.api.rest.spring1.model"}) // Para criar as tabelas automáticas
@@ -18,10 +20,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class Spring1Application {
+public class Spring1Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring1Application.class, args);
+	}
+	
+	/* Configuração de cross origin */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//WebMvcConfigurer.super.addCorsMappings(registry);
+		// registry.addMapping("/usuario/**").allowedMethods("POST","GET","PUT","DELETE")
+		registry.addMapping("//**")
+		.allowedMethods("*")
+		.allowedOrigins("*");
+		//.allowedOrigins("https://github.com/BlackCode7"); //Libera todos os methodos para o cliente blackcode
 	}
 
 }
