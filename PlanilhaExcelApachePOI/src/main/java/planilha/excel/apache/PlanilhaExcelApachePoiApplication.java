e6c3c81ad9a2f2fc749dfad51b4ab7c961747ba3
BlackCode7 <seven.metais@outlook.com>
2022-09-03 13:06:24 -0300

    Apache Poi

---
+++ PlanilhaExcelApachePOI/src/main/java/planilha/excel/apache/PlanilhaExcelApachePoiApplication.java
@@ -0,0 +1,13 @@
+package planilha.excel.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanilhaExcelApachePoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanilhaExcelApachePoiApplication.class, args);
+	}
+
+}
---
+++ PlanilhaExcelApachePOI/src/test/java/planilha/excel/apache/PlanilhaExcelApachePoiApplicationTests.java
@@ -0,0 +1,13 @@
+package planilha.excel.apache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanilhaExcelApachePoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
